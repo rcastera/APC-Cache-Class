49a17f9a0551e17d7b5bce8bf6826d67525f6797
Richard Castera <richard.castera@gmail.com>
2011-11-14 09:59:17 -0500

    Initial commit

---
+++ Class.ApcCache.php
@@ -0,0 +1,37 @@
+<?php
+
+class static ApcCache {
+	
+	public function __construct() {
+		
+	}
+
+	public function __destruct() {
+		
+	}
+
+	public function cacheInfo() {
+		
+	}
+
+	public function cacheExists() {
+		
+	}
+	
+	public function cacheStore() {
+		
+	}
+
+	public function cacheFetch() {
+		
+	}
+
+	public function cacheDelete() {
+		
+	}
+
+	public function cacheClear() {
+		
+	}
+
+}
